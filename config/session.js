679db53da2968635fa05fa74342b26e17c142b44
donvincenz0 <heroku@donvincenzo.com>
2014-05-27 01:04:40 +0900

    fixed session config file

--- config/session.js
+++ config/session.js
@@ -30,7 +30,7 @@ module.exports.session = {
   //
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  ttl: <redis session TTL in seconds>,
+  //ttl: <redis session TTL in seconds>,
   db: process.env.REDIS_DB,
   pass: process.env.REDIS_PASS
   prefix: 'sess:'
